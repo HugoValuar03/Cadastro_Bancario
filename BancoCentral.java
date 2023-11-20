c58b883d0885b6497f499898c04cfe3eb9fe52d8
Hugo Valuar Bailona <142545466+HugoValuar03@users.noreply.github.com>
2023-11-20 19:26:25 -0300

    Update BancoCentral.java

--- BancoCentral.java
+++ BancoCentral.java
@@ -28,6 +28,7 @@ public class BancoCentral {
 	}
 	
 	public static void incluirAgencia() {
+		String numero;
 		BancoCentral agencia = new BancoCentral();
 
 		List<Agencia> agencias = new ArrayList<Agencia>();
@@ -46,7 +47,7 @@ public class BancoCentral {
 		agencia.setConta(scan.next());
 		
 		System.out.println("Informe o numero da agencia:");
-		String numero = scan.next();
+		numero = scan.next();
 		
 		System.out.println("Informe o estado da agencia:");
 		String estado = scan.next();
@@ -65,11 +66,162 @@ public class BancoCentral {
 	}
 	
 	public static void alterarAgencia() {
-		
+		if(bancoMap.isEmpty()){
+			System.out.println("Nenhuma agência cadastrada");
+		} else {
+			System.out.println("Digite o nome da agência que deseja alterar: ");
+			String nomeAgencia = scan.next();
+
+			BancoCentral bancoCentral = bancoMap.get(nomeAgencia);
+			if (bancoCentral == null) {
+				System.out.println("Agencia não encontrada.");
+			} else {
+
+				System.out.println("O que deseja alterar");
+
+				List<BancoCentral> agenciasOrdenados = new ArrayList<>(bancoMap.values());
+
+				agenciasOrdenados.sort((o1, o2) -> o1.getNome().compareTo(o2.getNome()));
+				
+				System.out.println("Lista das agencias");
+				agenciasOrdenados.forEach(agencia -> System.out.println(agencia));
+				
+				
+				System.out.println(" ");
+				
+				System.out.println("1 - Nome da conta");
+				System.out.println("2 - Número da conta");
+				System.out.println("3 - Número da agência");
+				System.out.println("4 - Nome do Estado");
+				System.out.println("5 - Nome da Cidade");
+				System.out.println("6 - Endereço");
+
+				System.out.println();
+				
+				int opcao = scan.nextInt();
+
+				switch (opcao) {
+					case 1:
+						System.out.println("Digite o novo nome da agência: ");
+						String novoNome = scan.next();
+			
+						BancoCentral banco = bancoMap.get(nomeAgencia);
+						if (banco == null) {
+							System.out.println("Agência não encontrada");
+						} else {
+							bancoCentral.setNome(novoNome);
+							bancoMap.remove(nomeAgencia);
+							bancoMap.put(novoNome, banco);
+							System.out.println("O nome da Agência foi alterado para: " + novoNome);
+						}
+						break;
+						
+					case 2:
+						System.out.println("Digite o novo número da conta: ");
+						String novoNumeroConta = scan.next();
+
+						banco = bancoMap.get(nomeAgencia);
+						if (banco == null) {
+							System.out.println("Agência não encontrada.");
+						} else {
+							banco.setConta(novoNumeroConta);
+							bancoMap.put(nomeAgencia, banco);
+							System.out.println("O número da conta foi alterado para: " + novoNumeroConta);
+						}
+						break;
+						
+					case 3:
+						System.out.println("Digite o novo número da agência: ");
+						String novoNumeroAgencia = scan.next();
+						
+						banco = bancoMap.get(nomeAgencia);
+						if (banco == null) {
+							System.out.println("Agência não encontrada.");
+						} else {
+							List<Agencia> agencias = banco.getAgencia();
+							
+							for (Agencia agencia : agencias) {
+								agencia.setNumero(novoNumeroAgencia);
+							}
+							bancoMap.put(nomeAgencia, banco);
+							System.out.println("O número da agência foi alterado para: " + novoNumeroAgencia);
+						}
+					break;
+
+					case 4:
+						System.out.println("Digite o novo nome do estado: ");
+						String novoNomeEstado = scan.next();
+						
+						banco = bancoMap.get(nomeAgencia);
+						if (banco == null) {
+							System.out.println("Agência não encontrada.");
+						} else {
+							List<Agencia> agencias = banco.getAgencia();
+							
+							for (Agencia agencia : agencias) {
+								agencia.setEstado(novoNomeEstado);
+							}
+							bancoMap.put(nomeAgencia, banco);
+							System.out.println("O nome do estado foi alterado para: " + novoNomeEstado);
+						}
+					break;
+					
+					case 5:
+						System.out.println("Digite o novo nome da cidade: ");
+						String novoNomeCidade = scan.next();
+						
+						banco = bancoMap.get(nomeAgencia);
+						if (banco == null) {
+							System.out.println("Agência não encontrada.");
+						} else {
+							List<Agencia> agencias = banco.getAgencia();
+							
+							for (Agencia agencia : agencias) {
+								agencia.setCidade(novoNomeCidade);
+							}
+							bancoMap.put(nomeAgencia, banco);
+							System.out.println("O nome da cidade foi alterado para: " + novoNomeCidade);
+						}
+						
+						break;
+
+					case 6:
+						System.out.println("Digite o novo endereço: ");
+						String novoEndereco = scan.next();
+
+						banco = bancoMap.get(nomeAgencia);
+						if (banco == null) {
+							System.out.println("Agência não encontrada.");
+						} else {
+							banco.setEndereco(novoEndereco);
+							bancoMap.put(nomeAgencia, banco);
+							System.out.println("O número da conta foi alterado para: " + novoEndereco);
+						}
+						break;
+						
+					default:
+						System.out.println("Opção inexistente");
+						break;
+				}
+			}
+		}
 	}
 	
 	public static void excluirAgencia() {
-		
+		if (bancoMap.isEmpty()) {
+			System.out.println("Nenhuma agência cadastrada.");
+		} else {
+			System.out.println("Digite o nome da agência que deseja excluir: ");
+			String nomeAgencia = scan.next();
+	
+			BancoCentral bancoCentral = bancoMap.get(nomeAgencia);
+			if (bancoCentral == null) {
+				System.out.println("Agência não encontrada.");
+			} else {
+				bancoMap.remove(nomeAgencia);
+				System.out.println("Agência '" + nomeAgencia + "' excluída com sucesso.");
+			}
+		}
 	}
 	
 	public static void listarAgencia() {
@@ -98,7 +250,7 @@ public class BancoCentral {
 	
 	@Override
 	public String toString() {
-		return "nome: " + nome + ", conta: "+ conta + ", agencia: " + agencia.toString() + ", endereco: " + endereco + ", tipoAgencia: "
+		return "nome: " + nome + "\nconta: "+ conta + "\nagencia: " + agencia.toString() + "\nendereco: " + endereco + "\ntipoAgencia: "
 				+ tipoAgencia.getDescricao();
 	}
 	
